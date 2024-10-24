255301c2cda3ea061d8704d14699b882504bc483
maganga <88885384+Chrismaganga@users.noreply.github.com>
2024-10-24 22:21:56 +0200

    neon setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req: any, res: { writeHead: (arg0: number, arg1: { "Content-Type": string; }) => void; end: (arg0: any) => void; }) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
